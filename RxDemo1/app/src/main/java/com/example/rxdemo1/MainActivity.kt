bbcbeed2663fe22a385e4262b4aabf16feb942e1
artur.rodrigues <artur.rodrigues@tivit.com>
2020-02-03 15:34:31 -0300

    Commit inicial

---
+++ RxDemo1/app/src/main/java/com/example/rxdemo1/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.rxdemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.rxdemo1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setUpBinding()
+    }
+
+    private fun setUpBinding() {
+
+    }
+}
