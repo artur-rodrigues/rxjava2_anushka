d60b6213de66046091666f6b510e039f50e96b1b
artur.rodrigues <artur.rodrigues@tivit.com>
2020-02-04 15:11:47 -0300

    Fim Just

---
+++ JustDemo/app/src/main/java/com/example/justdemo/ContextUtils.kt
@@ -0,0 +1,18 @@
+package com.example.justdemo
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.StringRes
+
+fun log(msg: String, tag: String = "MY_TAG") {
+    Log.i(tag, msg)
+}
+
+fun Context.toast(msg: String, time: Int = Toast.LENGTH_LONG) {
+    Toast.makeText(this, msg, time).show()
+}
+
+fun Context.toast(@StringRes msg: Int, time: Int = Toast.LENGTH_LONG) {
+    Toast.makeText(this, msg, time).show()
+}
--- JustDemo/app/src/main/java/com/example/justdemo/MainActivity.kt
+++ JustDemo/app/src/main/java/com/example/justdemo/MainActivity.kt
@@ -5,15 +5,20 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.justdemo.databinding.ActivityMainBinding
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableObserver
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val greeting = "HelloFrom RxJava"
+//    private val greeting = arrayOf("Hello A", "Hello B", "Hello C", "Hello D")
     private lateinit var observable: Observable<String>
     private lateinit var observer: DisposableObserver<String>
+
+    private val disposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +27,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUpObservers() {
+        observable = Observable.just("Hello A", "Hello B", "Hello C", "Hello D")
 
+        disposable.add(
+            observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(getObserver())
+        )
+    }
+
+    private fun getObserver(): DisposableObserver<String> {
+        observer = object : DisposableObserver<String>() {
+            override fun onComplete() {
+                log("onComplete()")
+            }
+
+            override fun onNext(t: String) {
+                log("onNext()")
+            }
+
+            override fun onError(e: Throwable) {
+                log("onError()")
+            }
+
+        }
+
+        return observer
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        if (!observer.isDisposed) {
-            observer.dispose()
+        if (!disposable.isDisposed) {
+            disposable.dispose()
         }
     }
 }
