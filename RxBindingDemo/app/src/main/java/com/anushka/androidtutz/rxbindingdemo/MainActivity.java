39ed53e4ab579e167654cc509f1f89068f93c02e
artur.rodrigues <artur.rodrigues@tivit.com>
2020-02-06 15:20:01 -0300

    Commit inicial RxBinding

---
+++ RxBindingDemo/app/src/main/java/com/anushka/androidtutz/rxbindingdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.anushka.androidtutz.rxbindingdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText inputText;
+    private TextView viewText;
+    private Button clearButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputText = findViewById(R.id.etInputField);
+        viewText = findViewById(R.id.tvInput);
+        clearButton = findViewById(R.id.btnClear);
+
+    }
+}
